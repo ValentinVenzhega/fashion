8832f7270a4398249cbbe11b0993dbee11a0ac1f
ValentinVenяhega <valek917@yandex.ru>
2020-11-05 22:54:53 +0300

    commit4

--- script/script.js
+++ script/script.js
@@ -2,7 +2,10 @@
 
 const burgerMenu = document.querySelector('.menu__burger'),
       menu = document.querySelector('.menu'),
-      menuItem = document.querySelectorAll('.menu__i');
+      menuItem = document.querySelectorAll('.menu__i'),
+      love = document.querySelector('.love');
+
+
 
 
 //открытие и закртие меню и бургера
@@ -23,4 +26,16 @@ menuItem.forEach(function (e) {
 });
 
 
-
+// 1-й способ
+love.addEventListener('click', (e) => {
+   const target = e.target;
+   if(target.classList.contains('love')) {
+      love.innerHTML = `
+            <img src="./image/header/5.svg" alt="5">
+      `;
+   } else {
+      love.innerHTML = `
+            <img src="./image/header/4.svg"  class="love" alt="4">
+      `;
+   }
+});
